b6eb5164403bf5cbb8798e2e576973a45f09fbb5
yathi15 <mvyathishyathi@gmail.com>
2026-08-24 19:10:19 +0530

    fix login URL

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -18,7 +18,7 @@ function Login() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:5000/login",
+        "https://student-management-system-1-wbm2.onrender.com/login",
         {
           method: "POST",
 
